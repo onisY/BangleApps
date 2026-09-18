6a82d4d4107b882060b0e8def1c8a05d18d323ee
onY <148787164+onisY@users.noreply.github.com>
2026-09-18 09:16:36 +0900

    Mark observer delivery check in Orbclo Dev Orbit 0.16

--- apps/orbclo_dev_orbit/app.js
+++ apps/orbclo_dev_orbit/app.js
@@ -1,4 +1,4 @@
-/* Orbclo Dev Orbit 0.15 - observer marker stage */
+/* Orbclo Dev Orbit 0.16 - observer marker delivery check */
 (function(){
   var W=g.getWidth(),H=g.getHeight();
   var BLACK=0x0000,WHITE=0xFFFF,NAVY=0x000F,DARKBLUE=0x0008,CYAN=0x07FF,YELLOW=0xFFE0,ORANGE=0xFD20,RED=0xF800;
@@ -136,7 +136,7 @@
 
     var c=ms(t0,t1),s=ms(t1,t2),a=ms(t2,t3),e=ms(t3,t4),o=ms(t4,t5),h=ms(t5,t6),tot=ms(t0,t6);
     g.setColor(WHITE).setBgColor(BLACK).setFont("6x8",1).setFontAlign(0,0);
-    g.drawString("Az"+Math.round(sol.az)+" El"+Math.round(sol.el),W>>1,H-22);
+    g.drawString("OBS 0.16  Az"+Math.round(sol.az)+" El"+Math.round(sol.el),W>>1,H-22);
     g.drawString("C"+c+" S"+s+" A"+a+" E"+e+" O"+o+" H"+h+" T"+tot,W>>1,H-10);
     try{g.flip();}catch(err){}
     busy=false;
